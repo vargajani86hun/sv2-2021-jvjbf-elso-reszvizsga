8dc65d40923f392c96620b10d86e48c5a244bd1a
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-11 17:04:04 +0100

    In the string manipulation task, String and concat replaced with StringBuilder and append.

--- sv2-2021-jvjbf-elso-reszvizsga/src/main/java/stringmanipulation/StringManipulation.java
+++ sv2-2021-jvjbf-elso-reszvizsga/src/main/java/stringmanipulation/StringManipulation.java
@@ -2,10 +2,10 @@ package stringmanipulation;
 
 public class StringManipulation {
     public String everyEvenCharacter(String s) {
-        String result = "";
+        StringBuffer result = new StringBuffer();
         for (int i = 0; i < s.length(); i += 2){
-            result = result.concat(s.substring(i, i + 1));
+            result = result.append(s.substring(i, i + 1));
         }
-        return result;
+        return result.toString();
     }
 }
